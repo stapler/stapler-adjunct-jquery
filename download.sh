fa66853558ebaeebf2070319e4199740017fdd0a
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-11 21:18:04 -0700

    Imported 1.11.2

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dir=src/main/resources/org/kohsuke/stapler/jquery
-ver=1.8.3
+ver=1.11.2
 wget -O $dir/jquery.min.js  http://code.jquery.com/jquery-${ver}.min.js
 wget -O $dir/jquery.full.js http://code.jquery.com/jquery-${ver}.js
 mvn -B release:update-versions -DdevelopmentVersion=$ver-0-SNAPSHOT
